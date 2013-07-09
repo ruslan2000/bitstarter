8f0fa71cb00570db59bc42e0aedaf0080ff2c880
rusel <rusel2000@gmail.com>
2013-07-09 20:09:11 +0000

    final modification web.js

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var fs = require('fs');
-var buffer = new buffer(256);
+var buffer = new Buffer(fs.readFileSync('index.html'),'utf-8');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")));
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
